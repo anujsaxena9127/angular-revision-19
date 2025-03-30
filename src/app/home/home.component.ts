c22bf1703a9c2ec2560a07cd58caf3364fc6d6e2
Anuj Kumar <-g>
2025-03-30 12:05:04 +0530

    pasing data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
+import { GreetingComponent } from '../components/greeting/greeting.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,GreetingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  //passing data from parent to child compoenent
+  message1="hello greeting from home component"
 
 }
